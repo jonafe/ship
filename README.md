8c28423219e2291dd2370a6731d4e746c7b755f3
jonafe <42044390+jonafe@users.noreply.github.com>
2021-03-14 09:49:17 +0100

    Transfer shell scripts

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ship
+# Ship
 Transfer files with ease
+
+## Usage
+
+```console
+$ ship
+ship v0.1.0
+Transfer files with ease
+
+Usage: ship <file>
+```
---
+++ ship.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+  printf "%s\n" "ship v0.1.0"
+  printf "%s\n\n" "Transfer files with ease"
+  printf "%s\n" "Usage: ship <file>"
+  exit 1
+fi
+
+file_path="$1"
+file_name=$(basename "$file_path")
+
+file_name=${file_name/.sh/}
+cp "$file_path" "/usr/local/bin/$file_name"
+
+printf "%s\n" "Shipping complete 🌊"
